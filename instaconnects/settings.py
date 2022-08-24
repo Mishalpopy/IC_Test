f229eeb4db0672a3ff51b037c9e2d407b0efef7a
MishalPopy <c.mishal46@gmail.com>
2022-08-24 15:20:18 +0530

    registaration

--- instaconnects/settings.py
+++ instaconnects/settings.py
@@ -136,3 +136,6 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
